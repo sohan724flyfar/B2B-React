99945fd2f8dfddd018e3592c7f677dd40f5f0ed2
sohan724flyfar <sohan@flyfarint.com>
2022-05-15 09:58:03 +0600

    Add home nav bar

---
+++ src/Components/Home/Home/Home.js
@@ -0,0 +1,15 @@
+import { Box } from "@mui/system";
+import React from "react";
+import HomeNav from "../HomeNav/HomeNav";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <Box>
+      <HomeNav />
+    </Box>
+  );
+};
+
+export default Home;
