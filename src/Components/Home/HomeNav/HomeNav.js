2f6038cee2d96f7680919ed075d3dafcd9333022
sohan724flyfar <102467622+sohan724flyfar@users.noreply.github.com>
2022-05-18 09:16:20 +0600

    code changes by sohan

--- src/Components/Home/HomeNav/HomeNav.js
+++ src/Components/Home/HomeNav/HomeNav.js
@@ -11,7 +11,7 @@ import Avatar from "@mui/material/Avatar";
 import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
-import AdbIcon from "@mui/icons-material/Adb";
+// import AdbIcon from "@mui/icons-material/Adb";
 
 //CSS file
 import "./HomeNav.css";
@@ -42,7 +42,7 @@ const HomeNav = () => {
       <AppBar position="static" className="home-nav">
         <Container maxWidth="xl">
           <Toolbar disableGutters>
-            <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} />
+            {/* <AdbIcon sx={{ display: { xs: "none", md: "flex" }, mr: 1 }} /> */}
             <Typography
               variant="h6"
               noWrap
@@ -97,7 +97,7 @@ const HomeNav = () => {
                 ))}
               </Menu>
             </Box>
-            <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} />
+            {/* <AdbIcon sx={{ display: { xs: "flex", md: "none" }, mr: 1 }} /> */}
             <Typography
               variant="h5"
               noWrap
